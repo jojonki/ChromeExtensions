91f0913fa5dc895fd3abd133b896ecbdbb133caa
jojonki <jonki.acg@gmail.com>
2014-02-09 16:13:52 +0900

    add FaceBook Ad killer

---
+++ extentions/GoodByeFaceBookAd/js/main.js
@@ -0,0 +1,11 @@
+window.onload = DisableAdTag;
+
+function DisableAdTag() {
+	console.log('run DisableAdTag');
+    var sideAd = document.getElementById('pagelet_side_ads');
+    if(sideAd && sideAd.parentNode) sideAd.parentNode.removeChild(sideAd);
+
+    var mainAd = document.getElementById('pagelet_ego_pane');
+    if(mainAd && mainAd.parentNode) mainAd.parentNode.removeChild(mainAd);
+}
+
